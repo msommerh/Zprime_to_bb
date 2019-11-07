365041adc5f08b80715ac6aa982ef9492f3764dd
Manuel Sommerhalder <manuel.sommerhalder@uzh.ch>
2019-11-07 13:37:08 +0100

    working interpolation and limits plot

--- combine.sh
+++ combine.sh
@@ -11,37 +11,33 @@ option=""
 #option="-H ProfileLikelihood"
 
 
-#if [[ "$1" != "-m" ]] || [[ "$2" == "" ]] || [[ "$3" == "" ]]
-#then
-#    echo Select a method and a filter:
-#    echo "  -m:          " $(ls datacards/)
-#    return
-#fi
-
 higgsCombine() {
     inputfile=$1
     outputfile=$2
-    mass=$(echo $inputfile | sed s/2016// | sed s/2017// | sed s/2018// | sed s/run2// |tr -dc '0-9') #delete every character except the numbers. First removes the year 
-    #echo Running $method mass $mass on $inputfile...
-    echo "Input file: ${inputfile}; Output file: ${outputfile}"
-
-    > $outputfile
-    #if echo "$option" | grep -q "blind"; then 
-    #  echo '1' >> $outputfile
-    #fi
-    combine -M AsymptoticLimits -d $inputfile -m $mass | grep -e Observed -e Expected | awk '{print $NF}' >> $outputfile #i.e. take the output of combine, select the lines containing "Observed" and "Expected" via grep, select the last field via awk and append it to the outputfile
+    lower_mass=$3
+    upper_mass=$4
+    mass=$(echo $inputfile | sed s/2016// | sed s/2017// | sed s/2018// | sed s/run2// |tr -dc '0-9') #delete every character except the numbers. First removes the year
+    if [[ $mass -gt $lower_mass ]] && [[ $mass -le $upper_mass ]]; then
+        echo "Input file: ${inputfile}; Output file: ${outputfile}"
+        > $outputfile
+        #if echo "$option" | grep -q "blind"; then 
+        #  echo '1' >> $outputfile
+        #fi
+        combine -M AsymptoticLimits -d $inputfile -m $mass | grep -e Observed -e Expected | awk '{print $NF}' >> $outputfile #i.e. take the output of combine, select the lines containing "Observed" and "Expected" via grep, select the last field via awk and append it to the outputfile
+    fi
 }
 
 
-for card in datacards/$1/combined/$2*.txt
-do
-    #analysis=$(basename $(dirname $card))
-    #signal=$(basename $card .txt)
-#    echo Running with method $2 on $signal
-    #higgsCombine $signal $2 &
-    #output=$(echo $card | sed s:datacards/combined:combine/limits: | sed s/combined_//g)
-    output=$(echo $card | sed s:datacards/:combine/limits/: | sed s:combined/combined_::g)
-    higgsCombine $card $output &
+for low_mass in 1000 2000 3000 4000 5000 6000 7000; do
+    let high_mass=$low_mass+1000
+
+    echo "low_mass=${low_mass}, high_mass=${high_mass}"
+
+    for card in datacards/$1/combined/$2*.txt; do
+        output=$(echo $card | sed s:datacards/:combine/limits/: | sed s:combined/combined_::g)
+        higgsCombine $card $output $low_mass $high_mass &
+    done
+    wait
 done
 
 ## Clean
@@ -53,12 +49,3 @@ rm mlfit*.root
 
 echo -e "\e[00;32mAll clear\e[00m"
 
-# python $CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/leeFromUpcrossings.py bands.root graph 1000 4500 --fit=best
-
-# To derive Impacts (https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsWG/SWGuideNonStandardCombineUses#Nuisance_parameter_impacts):
-# text2workspace.py datacards/dijet/XVHah_M2000.txt
-# produces .root file in the same directory of the datacard
-# combine -M MultiDimFit -n _initialFit_Test --algo singles --redefineSignalPOIs r --robustFit 1 -m 2000 -d XVHah_M2000.root
-# combineTool.py -M Impacts -d XVHah_M2000.root -m 2000 --robustFit 1 --doFits --parallel 10
-# combineTool.py -M Impacts -d XVHah_M2000.root -m 2000 -o impacts.json
-# plotImpacts.py -i impacts.json -o impacts
