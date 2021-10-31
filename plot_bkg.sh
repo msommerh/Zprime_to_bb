929272cb0793cf2df4ecc9c01b2e966cbaaff6c5
Manuel Sommerhalder <manuel.sommerhalder@uzh.ch>
2021-10-31 16:14:18 +0100

    adding plot macros for consistency

---
+++ plot_bkg.sh
@@ -0,0 +1,11 @@
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2016_bb.root  -y 2016 -c 17.5 -d 29 -w "Zprime_2016" -m "Bkg_2016_bb" --category bb -o plots/medium/data_2016/Bkg_new_bb.pdf
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2016_bq.root  -y 2016 -c 35.7 -d 33 -w "Zprime_2016" -m "Bkg_2016_bq" --category bq -o plots/medium/data_2016/Bkg_new_bq.pdf
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2016_mumu.root  -y 2016 -c 19.1 -d 27 -w "Zprime_2016" -m "Bkg_2016_mumu" --category mumu -o plots/medium/data_2016/Bkg_new_mumu.pdf
+
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2017_bb.root  -y 2017 -c 30.3 -d 27 -w "Zprime_2017" -m "Bkg_2017_bb" --category bb -o plots/medium/data_2017/Bkg_new_bb.pdf
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2017_bq.root  -y 2017 -c 37.9 -d 34 -w "Zprime_2017" -m "Bkg_2017_bq" --category bq -o plots/medium/data_2017/Bkg_new_bq.pdf
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2017_mumu.root  -y 2017 -c 20.6 -d 29 -w "Zprime_2017" -m "Bkg_2017_mumu" --category mumu -o plots/medium/data_2017/Bkg_new_mumu.pdf
+
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2018_bb.root  -y 2018 -c 27.9 -d 29 -w "Zprime_2018" -m "Bkg_2018_bb" --category bb -o plots/medium/data_2018/Bkg_new_bb.pdf
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2018_bq.root  -y 2018 -c 19.3 -d 34 -w "Zprime_2018" -m "Bkg_2018_bq" --category bq -o plots/medium/data_2018/Bkg_new_bq.pdf
+python Bkg_Fitter_separate_macro.py -i workspace/medium/data_2018_mumu.root  -y 2018 -c 26.6 -d 30 -w "Zprime_2018" -m "Bkg_2018_mumu" --category mumu -o plots/medium/data_2018/Bkg_new_mumu.pdf
